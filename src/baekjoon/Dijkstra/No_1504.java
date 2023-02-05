bbb11b215511f6af4cac3cd984ea751e7cfc11c2
SehyeonKang <gedfgrdt@naver.com>
2023-02-05 21:51:17 +0900

    20230205 commit

---
+++ src/baekjoon/Dijkstra/No_1504.java
@@ -0,0 +1,110 @@
+package baekjoon.Dijkstra;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class No_1504 {
+
+    private static class Node implements Comparable<Node> {
+
+        int end, weight;
+
+        public Node(int end, int weight) {
+            this.end = end;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return weight - o.weight;
+        }
+    }
+
+    static final int INF = 200000000;
+    static int N, E, v1, v2;
+    static ArrayList<ArrayList<Node>> list;
+    static int[] dist;
+    static boolean[] check;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        list = new ArrayList<>();
+        dist = new int[N + 1];
+        check = new boolean[N + 1];
+
+        for (int i = 0; i <= N; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int dist = Integer.parseInt(st.nextToken());
+
+            list.get(start).add(new Node(end, dist));
+            list.get(end).add(new Node(start, dist));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        v1 = Integer.parseInt(st.nextToken());
+        v2 = Integer.parseInt(st.nextToken());
+
+        int result1 = 0;
+        result1 += dijkstra(1, v1);
+        result1 += dijkstra(v1, v2);
+        result1 += dijkstra(v2, N);
+
+        int result2 = 0;
+        result2 += dijkstra(1, v2);
+        result2 += dijkstra(v2, v1);
+        result2 += dijkstra(v1, N);
+
+        int answer = (result1 >= INF && result2 >= INF) ? -1 : Math.min(result1, result2);
+
+        System.out.println(answer);
+
+        br.close();
+    }
+
+    private static int dijkstra(int start, int end) {
+
+        Arrays.fill(dist, INF);
+        Arrays.fill(check, false);
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start, 0));
+        dist[start] = 0;
+
+        while (!pq.isEmpty()) {
+
+            Node curNode = pq.poll();
+            int cur = curNode.end;
+
+            if (!check[cur]) {
+                check[cur] = true;
+
+                for (Node node : list.get(cur)) {
+                    if (dist[node.end] > dist[cur] + node.weight) {
+                        dist[node.end] = dist[cur] + node.weight;
+                        pq.add(new Node(node.end, dist[node.end]));
+                    }
+                }
+            }
+        }
+
+        return dist[end];
+    }
+
+}
